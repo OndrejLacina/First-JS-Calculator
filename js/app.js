ce44e4b921c84f54a5a0c3b115cf3a1e1e4e19c2
Ondrej Lacina <laci7@seznam.cz>
2025-03-02 22:10:53 +0100

    final version 1.0

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 const screenDisplay = document.querySelector(".screen");
 const buttons = document.querySelectorAll("button");
-
 console.log(buttons);
 
+// memory - safe what we clicked
+let calculation = [];
+let accumulativeCalculation;
+
+// action - clicking on number
 function calculate(button) {
-  console.log("clicked", button);
+  const value = button.textContent;
+  if (value === "CLEAR") {
+    calculation = [];
+    screenDisplay.textContent = ".";
+  } else if (value === "=") {
+    console.log(accumulativeCalculation);
+    screenDisplay.textContent = eval(accumulativeCalculation); //eval only for practice - could be security threat
+  } else {
+    calculation.push(value); // push values to memory
+    accumulativeCalculation = calculation.join("");
+    screenDisplay.textContent = accumulativeCalculation;
+  }
 }
 
 buttons.forEach((button) =>
